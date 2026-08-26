fe3b311916283e084f2b8165362de3004d7af8a9
Josh Morris <97921224+joshtrus@users.noreply.github.com>
2026-08-25 21:56:19 -0400

    Add: depth-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-4.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #Assumptions
+        #-------------------------------------
+        #longest path either left or right
+        #tree with only root returns depth 1
+
+
+        #Clarifying Questions
+        #-------------------------------------
+        # Does amount of children affect depth
+
+        #Approach
+        #-------------------------------------
+        #recursive approach
+        #call function on left and right + 1
+        #return max of whichever
+        #Time complexity O(n) Space complexity of O(1) because nothing is stored. happening in line
+
+        #Better Approach
+        #-------------------------------------
+
+        if not root:
+            return 0
+        
+        left = 1 + self.maxDepth(root.left)
+        right = 1 + self.maxDepth(root.right)
+
+        return max(left, right)
+
+        
